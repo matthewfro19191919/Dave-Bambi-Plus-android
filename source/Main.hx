55879b748c8f4b64730598529de122ddedb39b63
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-12 08:53:42 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -8,7 +8,7 @@ import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
-import flixel.system.FlxSound;
+import flixel.sound.FlxSound;
 import flixel.FlxG;
 import flixel.util.FlxColor;
 
