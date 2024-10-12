352bcddbb36b5c226ff4e89924a79adac4745df6
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-12 06:50:45 -0400

    Update window.bat

--- setup/window.bat
+++ setup/window.bat
@@ -4,21 +4,23 @@ cd ..
 @echo on
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install hxcpp > /dev/null
-haxelib install lime 8.0.2
+haxelib install hxcpp > nul
+haxelib install lime 8.0.0
 haxelib install openfl 9.2.0
-haxelib --never install flixel 5.3.1
-haxelib run lime setup flixel
-haxelib run lime setup
+haxelib install flixel 4.11.0 
 haxelib install flixel-tools 1.5.1 --quiet
 haxelib install flixel-ui 2.5.0 --quiet
 haxelib install flixel-addons 3.1.1 --quiet
-haxelib install tjson --quiet
-haxelib install hxCodec --quiet
-haxelib git luajit https://github.com/nebulazorua/linc_luajit --quiet
-haxelib install hscript 2.4.0 --quiet
-haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex --quiet
-haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc --quiet
-haxelib install hxcpp-debug-server --quiet
+haxelib install tjson
+haxelib install hxjsonast
+haxelib install hxCodec 2.5.1
+haxelib install linc_luajit
+haxelib install hscript
+haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib install hxcpp-debug-server
 echo Finished!
 pause
