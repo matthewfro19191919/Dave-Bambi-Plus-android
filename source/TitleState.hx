52a5b7861fe6e4fa18c8a66810994212a2b1bc4a
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-12 08:50:56 -0400

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -12,7 +12,7 @@ import flixel.group.FlxGroup;
 import flixel.input.gamepad.FlxGamepad;
 import flixel.math.FlxPoint;
 import flixel.math.FlxRect;
-import flixel.system.FlxSound;
+import flixel.sound.FlxSound;
 import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
