0ca9fea16ef2d015bf1d7309e041a0d82fd50764
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-17 16:04:04 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -5,25 +5,22 @@
 cd ..
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install hxcpp > /dev/null
-haxelib install lime
-haxelib install openfl
-haxelib --never install flixel
-haxelib run lime setup flixel
-haxelib run lime setup
-haxelib install flixel-tools
-haxelib install flixel-ui
-haxelib install flixel-addons
+haxelib install hxcpp > nul
+haxelib install lime 8.0.0
+haxelib install openfl 9.2.0
+haxelib install flixel 4.11.0 
+haxelib install flixel-tools 1.5.1 --quiet
+haxelib install flixel-ui 2.5.0 --quiet
+haxelib install flixel-addons 3.1.1 --quiet
 haxelib install tjson
 haxelib install hxjsonast
-haxelib install hxCodec
-haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
+haxelib install hxCodec 2.5.1
+haxelib install linc_luajit
 haxelib install hscript
 haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
 haxelib git faxe https://github.com/uhrobots/faxe
-haxelib git polymod https://github.com/MasterEric/polymod.git
-haxelib git hxCodec https://github.com/polybiusproxy/hxCodec
+haxelib git polymod https://github.com/larsiusprime/polymod.git
 haxelib install hxcpp-debug-server
 echo Finished!
